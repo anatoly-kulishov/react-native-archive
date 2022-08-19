fedd14ecbaba7cca2bf5d6771783824c1a01ba5f
Anatoly.Kulishov <Anatoly.Kulishov@waveaccess.ru>
2022-08-19 15:56:46 +0600

    1.0.2

--- src/screens/Analytics.screen.tsx
+++ src/screens/Analytics.screen.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import { groupBy } from 'lodash';
+import { VictoryPie } from 'victory-native';
 import { StyleSheet, View } from 'react-native';
-import { AppText } from '../components/AppText';
+import { useAppContext } from '../App.provider';
+import { theme } from '../shared/theme';
 
 export const Analytics: React.FC = () => {
+  const appContext = useAppContext();
+
+  const data = Object.entries(groupBy(appContext.moodList, 'mood.emoji')).map(
+    ([key, value]) => ({
+      x: key,
+      y: value.length,
+    }),
+  );
+
   return (
     <View style={styles.container}>
-      <AppText fontFamily="bold">Analytics</AppText>
+      <VictoryPie
+        labelRadius={80}
+        radius={150}
+        innerRadius={50}
+        colorScale={[
+          theme.colorPurple,
+          theme.colorLavender,
+          theme.colorBlue,
+          theme.colorGrey,
+          theme.colorWhite,
+        ]}
+        style={{ labels: { fontSize: 30 } }}
+        data={data}
+      />
     </View>
   );
 };
@@ -13,6 +38,7 @@ export const Analytics: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
